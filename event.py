1a2107dc69bd1308ec4f545520812bd5402cdbc7
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-29 11:18:43 +0530

    Update event.py

--- event.py
+++ event.py
@@ -8,9 +8,9 @@ if len(sys.argv) == 5:
     organiser_name = sys.argv[4]
 else:
         event_name = "Birthday"
-    venue = "Hubli"
-    date = "1/12/2025"
-    organiser_name = "Keerti"
+        venue = "Hubli"
+        date = "1/12/2025"
+        organiser_name = "Keerti"
 
 print("Event Name:", event_name)
 print("Venue:", venue)
