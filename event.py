760f16879b8417262c1d260c2d7b27369372fac6
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-29 11:15:31 +0530

    Update event.py

--- event.py
+++ event.py
@@ -15,8 +15,9 @@ else:
     date = "1/12/2025"
     organiser_name = "Keerti"
 
-    print("Event Name: ", event_name)
-    print("Venue: ", venue)
-    print("Date: ", date)
-    print("Organiser Name: ", organiser_name)
+
+print("Event Name: ", event_name)
+print("Venue: ", venue)
+print("Date: ", date)
+print("Organiser Name: ", organiser_name)
 
