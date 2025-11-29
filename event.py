47d32933a70cbf0d3a6836f676bc70da0c336e57
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-29 10:57:38 +0530

    Update event.py

--- event.py
+++ event.py
@@ -4,7 +4,7 @@ if len(sys.argv) != 5:
     print("Usage: python event.py <event_name> <venue> <date> <organiser_name>")
     sys.exit(1)
     
-    event_name = string(sys.arge[1])
+    event_name = string(sys.argv[1])
     venue = (sys.argv[2])
     date = string(sys.argv[3])
     organiser_name = string(sys.argv[4])
