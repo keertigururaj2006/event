d1388b376e245e71fd966527312198be7b0eda96
yashvanttalawar6-creator <yashvanttalawar6@gmail.com>
2025-11-29 10:38:15 +0530

    Saving File

---
+++ event.py
@@ -0,0 +1,27 @@
+import sys
+
+if len(sys.argv) != 5:
+    print("Usage: python event.py <event_name> <venue> <date> <organiser_name>")
+    sys.exit(1)
+    
+    event_name = string(sys.arge[1])
+    venue = (sys.argv[2])
+    date = string(sys.argv[3])
+    organiser_name = string(sys.argv[4])
+    
+    print("Event Name: ", event_name)
+    print("Venue: ", venue)
+    print("Date: ", date)
+    print("Organiser Name: ", organiser_name)
+    
+else:
+    event_name = "Birthday"
+    venue = "Hubli"
+    date = "1/12/2025"
+    organiser_name = "Keerti"
+    
+    print("Event Name: ", event_name)
+    print("Venue: ", venue)
+    print("Date: ", date)
+    print("Organiser Name: ", organiser_name)
+
