74cd0afffeb0645295107627a96ede0c0648459c
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-29 11:17:30 +0530

    Update event.py

--- event.py
+++ event.py
@@ -1,23 +1,18 @@
 import sys
 
-if len(sys.argv) != 5:
-    print("Usage: python event.py <event_name> <venue> <date> <organiser_name>")
-    sys.exit(1)
-    
+if len(sys.argv) == 5:
+    # Read values from command line
     event_name = sys.argv[1]
     venue = sys.argv[2]
     date = sys.argv[3]
-    organiser_name = sys.argv[4]    
+    organiser_name = sys.argv[4]
 else:
-    print(" ")
-    event_name = "Birthday"
+        event_name = "Birthday"
     venue = "Hubli"
     date = "1/12/2025"
     organiser_name = "Keerti"
 
-
-print("Event Name: ", event_name)
-print("Venue: ", venue)
-print("Date: ", date)
-print("Organiser Name: ", organiser_name)
-
+print("Event Name:", event_name)
+print("Venue:", venue)
+print("Date:", date)
+print("Organiser Name:", organiser_name)
