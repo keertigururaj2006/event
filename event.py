3d3179bb59feed8e61be49892f09a50ed2fb4027
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-29 11:29:45 +0530

    Update event.py

--- event.py
+++ event.py
@@ -1,19 +1,16 @@
 import sys
 
-if len(sys.argv) != 5:
-    event_name = sys.argv[1]
-    venue = sys.argv[2]
-    date = sys.argv[3]
-    organiser_name = sys.argv[4]
-    print("Event Name:", event_name)
-    print("Venue:", venue)
-    print("Date:", date)
-    print("Organiser Name:", organiser_name)
+if len(sys.argv) == 5:
+    print("Event Name:", sys.argv[1])
+    print("Venue:", sys.argv[2])
+    print("Date:", sys.argv[3])
+    print("Organiser Name:", sys.argv[4])
+
 else:
-    event_name = "Birthday"
-    venue = "Hubli"
-    date = "1/12/2025"
-    organiser_name = "Keerti"
+event_name = "Birthday"
+venue = "Hubli"
+date = "1/12/2025"
+organiser_name = "Keerti"
 
 print("Event Name:", event_name)
 print("Venue:", venue)
